0904d98ce74407e63c77c442f86e91234873efff
Adhiban Ganesh N <adhibangn@Adhibans-MacBook-Air.local>
2024-10-27 17:36:30 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// app.js
+const express = require('express');
+const app = express();
+
+app.get('/status', (req, res) => {
+    res.json({ status: 'API is running successfully!' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
